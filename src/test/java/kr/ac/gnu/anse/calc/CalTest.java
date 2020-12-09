3f414a3079b90125e95d6831a4ef82c058fb340c
kimeunsu96 <70626828+kimeunsu96@users.noreply.github.com>
2020-12-09 16:00:41 +0900

    Add files via upload

---
+++ src/test/java/kr/ac/gnu/anse/calc/CalTest.java
@@ -0,0 +1,16 @@
+package kr.ac.gnu.anse.calc;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalTest {
+   
+	@Test
+	public void testAdd() {
+		Calc c = new Calc();
+		assertEquals(30, c.add(10,20));
+	}
+}
